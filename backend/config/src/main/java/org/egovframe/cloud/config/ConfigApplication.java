2ac451767aeb498d034f0029caad562364c45113
wcson <itnj.wcson@gmail.com>
2023-11-27 16:57:55 +0900

    일단 주석

--- backend/config/src/main/java/org/egovframe/cloud/config/ConfigApplication.java
+++ backend/config/src/main/java/org/egovframe/cloud/config/ConfigApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+@EnableConfigServer // 컨피크 서버 추가 !
 @SpringBootApplication
 public class ConfigApplication {
 
